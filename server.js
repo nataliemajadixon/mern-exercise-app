ce5dd417c9627e770d49ebe3de85b0149cb0c920
natolay <ndixon@seattleu.edu>
2022-12-20 21:22:34 +0100

    created router and mongo routes

--- server.js
+++ server.js
@@ -26,11 +26,7 @@ mongoose
         
     })
 
-//  const exercisesRouter = require('./routes/exercises');
-//  const usersRouter = require('./routes/users')
-
-//  app.use('/exercises', exercisesRouter);
-//  app.use('/users', usersRouter);
+    
 
 
 
